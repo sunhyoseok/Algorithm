3f282ea7fa8dba092f12e048f656f4da65ca7104
sunhyoseok <sunhyoseok66@gmail.com>
2024-04-07 00:44:48 +0900

    test

--- Selection_sort.dSYM/Contents/Assignment1_PriorityQueue.c
+++ Selection_sort.dSYM/Contents/Assignment1_PriorityQueue.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <stdint.h>
 #define HEAP_MAX 500000
+
+
 
 //  Heap implementation
 
---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    int i;
+    scanf("%d", &i);
+    printf("%d", i);
+    return 0;
+}
